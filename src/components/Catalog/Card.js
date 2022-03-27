2e56a5260bcab42b6799004b37b63c42d1c35b3b
delenjer <delenjer@gmail.com>
2022-03-27 22:02:47 +0300

    Fixed routing.

---
+++ src/components/Catalog/Card.js
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import Typography from '@mui/material/Typography';
+import { imgCard, skeletonStyle, titleCard } from '../../helper/helper';
+import Skeleton from '@mui/material/Skeleton';
+
+export const Card = ({ art, isLoadImg, setLoadImg }) => {
+
+  useEffect(() => {
+    const images = [...document.querySelectorAll('.card-img')];
+
+    if(images) {
+      return images.every(image => image.addEventListener("load", () => setLoadImg(true)));
+    }
+  }, [setLoadImg]);
+
+  return (
+    <div>
+      <Typography
+        variant="h5"
+        sx={titleCard}
+      >
+        {art.title}
+      </Typography>
+
+      <Link
+        to={`details/${art.objectNumber}`}
+        className="card-link"
+      >
+        <div className={!isLoadImg ? 'img-box img-box-height' : 'img-box'}>
+          <img
+            src={!art.webImage ? '' : `${art.webImage.url}`}
+            alt={art.title}
+            loading="lazy"
+            style={imgCard}
+            className="card-img"
+          />
+
+          {!isLoadImg && <Skeleton variant="rectangular" sx={skeletonStyle} />}
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  title: PropTypes.string,
+  webImage: PropTypes.object,
+  url: PropTypes.string,
+  objectNumber: PropTypes.string,
+  isLoadImg: PropTypes.bool,
+  setLoadImg: PropTypes.func,
+  handleClick: PropTypes.func,
+}
---
+++ src/components/Details/Details.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Link, useParams } from 'react-router-dom';
+
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Box from '@mui/material/Box';
+
+import { catalogDetails } from '../../api/api';
+import {iconBox, spinnerBox} from '../../helper/helper';
+import { DetailsContent } from './DetailsContent';
+import CircularProgress from '@mui/material/CircularProgress';
+
+export const Details = () => {
+  const [ details, setDetails ] = useState({});
+  const [ isLoading, setLoading ] = useState(false);
+  let { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    catalogDetails(id)
+      .then(data => {
+        setDetails(data);
+        setLoading(false);
+      })
+      .catch(() => setDetails({}));
+  }, [id]);
+
+  return (
+    <div className="container">
+      <Box component="div" mb={6}>
+        <Link to="/" className="link-back">
+          <Box sx={iconBox} component="span">
+            <ArrowBackIcon />
+          </Box>
+
+          Back
+        </Link>
+      </Box>
+      {
+        isLoading ? (
+          <Box sx={spinnerBox}>
+            <CircularProgress />
+          </Box>
+        ) : (
+          <DetailsContent detail={details.data} />
+        )
+      }
+    </div>
+  );
+}
+
+Details.propTypes = {
+  objectNumber: PropTypes.string,
+}
